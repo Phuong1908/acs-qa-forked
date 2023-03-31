a5e1589b7f1a297ee00b7fdd7782d4909eaccfcf
phuongn <thuphuong.nguyen.cntt@gmail.com>
2023-03-31 17:32:20 +0700

    glove2word2vec is deprecated

--- create_glove_binary.py
+++ create_glove_binary.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.test.utils import get_tmpfile
 from gensim.models import KeyedVectors
 
@@ -15,6 +14,5 @@ if __name__ == "__main__":
     # Create GloVe binary file
     glove_file = os.path.join(args.data_dir, 'original/Glove/glove.840B.300d.txt')
     tmp_file = get_tmpfile("glove.840B.300d.w2v.txt")
-    _ = glove2word2vec(glove_file, tmp_file)
     model = KeyedVectors.load_word2vec_format(tmp_file)
     model.wv.save_word2vec_format(os.path.join(args.data_dir, "original/Glove/glove.840B.300d.bin"), binary=True)
