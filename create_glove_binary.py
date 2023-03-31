8a70dbc45f30e2db47b08f3989ddc5ad274052f7
phuongn <thuphuong.nguyen.cntt@gmail.com>
2023-04-01 00:35:26 +0700

    Experiment with datapath

--- create_glove_binary.py
+++ create_glove_binary.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
 
 
 if __name__ == "__main__":
@@ -12,5 +13,5 @@ if __name__ == "__main__":
 
     # Create GloVe binary file
     glove_file = os.path.join(args.data_dir, 'original/Glove/glove.840B.300d.txt')
-    model = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True)
+    model = KeyedVectors.load_word2vec_format(datapath(glove_file), binary=False)
     model.save_word2vec_format(os.path.join(args.data_dir, "original/Glove/glove.840B.300d.bin"), binary=True)
