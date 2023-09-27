ed9d5be4947ef8a98463a06d657408ae170c0c2e
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-09-27 21:55:45 +0700

    [f] Implement scripts for generating results with SQuAD & Wiki data

---
+++ experiments_4_QG_gen_gpt2_wiki.sh
@@ -0,0 +1,12 @@
+# wiki data
+output_path="/content/acsqg/Datasets/processed/Wiki10000/"
+data_file_prefix="wiki10000"
+st_idx=0
+ed_idx=50000
+PYTHONIOENCODING=utf-8 python3 QG_gpt2_generate.py  \
+    --model_type gpt2 \
+    --model_name_or_path /content/acsqg/output/QG/gpt2_question_generation/ \
+    --filename "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.pkl" \
+    --filecache "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.cache.qg.gpt2.pth" \
+    --data_type augmented_sents \
+    --output_file "$output_path${data_file_prefix}.qa.${st_idx}_${ed_idx}.qg.generated.gpt2.json"
