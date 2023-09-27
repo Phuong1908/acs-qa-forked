7995dce6734ff1330226c1d591aba518ae676b83
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-09-27 22:30:20 +0700

    [b] Fix incorrect model path

--- experiments_4_QG_gen_gpt2_wiki.sh
+++ experiments_4_QG_gen_gpt2_wiki.sh
@@ -5,7 +5,7 @@ st_idx=0
 ed_idx=50000
 PYTHONIOENCODING=utf-8 python3 QG_gpt2_generate.py  \
     --model_type gpt2 \
-    --model_name_or_path /content/acsqg/output/QG/gpt2_question_generation/ \
+    --model_name_or_path /content/acsqg/Datasets/output/QG/gpt2_question_generation/ \
     --filename "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.pkl" \
     --filecache "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.cache.qg.gpt2.pth" \
     --data_type augmented_sents \
