fdeef19045f704769d8f88213eb3b20ca6ef4a96
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-09-28 14:07:12 +0700

    [c] Save cache to gdrive

--- experiments_4_QG_gen_gpt2_wiki.sh
+++ experiments_4_QG_gen_gpt2_wiki.sh
@@ -7,6 +7,6 @@ PYTHONIOENCODING=utf-8 python3 QG_gpt2_generate.py  \
     --model_type gpt2 \
     --model_name_or_path /content/acsqg/Datasets/output/QG/gpt2_question_generation/ \
     --filename "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.pkl" \
-    --filecache "$output_path${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.cache.qg.gpt2.pth" \
+    --filecache "/content/gdrive/MyDrive/QA_Generation/Datasets/processed/${data_file_prefix}.sentences.augmented.${st_idx}_${ed_idx}.cache.qg.gpt2.pth" \
     --data_type augmented_sents \
     --output_file "$output_path${data_file_prefix}.qa.${st_idx}_${ed_idx}.qg.generated.gpt2.json"
